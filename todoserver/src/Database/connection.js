25056ba20a2fb8b39f92555f0c8cc7986c9e2fd4
Kalaisharma <99097741+Kalaisharma@users.noreply.github.com>
2025-07-16 15:17:22 +0530

    Refactor .gitignore, update MongoDB connection logic, and modify CORS settings

--- todoserver/src/Database/connection.js
+++ todoserver/src/Database/connection.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/TodoList");
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection failed:", err.message);
--- todoserver/src/index.js
+++ todoserver/src/index.js
@@ -4,7 +4,11 @@ const taskRoutes = require("./Routes/taskRoutes");
 const app = express();
 const connectDB = require("./Database/connection");
 connectDB();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://kalai-todolist.netlify.app/", credentials: true })
+);
+
 app.use(express.json());
 app.use("/api", taskRoutes);
 app.listen(process.env.PORT || 5000, () => {
